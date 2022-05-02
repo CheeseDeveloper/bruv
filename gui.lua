e9d35b670982ff4bab8a013198a3dfba40bc89c4
crlMinXx <86245870+CheeseDeveloper@users.noreply.github.com>
2022-05-02 16:54:41 -0500

    Create gui.lua

---
+++ gui.lua
@@ -0,0 +1,23 @@
+local ScreenGui = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local UICorner = Instance.new("UICorner")
+local UIListLayout = Instance.new("UIListLayout")
+
+--Properties:
+
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+Frame.Parent = ScreenGui
+Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame.BorderColor3 = Color3.fromRGB(27, 42, 53)
+Frame.Position = UDim2.new(0.378818721, 0, 0.166944906, 0)
+Frame.Size = UDim2.new(0.283660412, 0, 0.665943384, 0)
+
+UICorner.CornerRadius = UDim.new(0.0199999996, 0)
+UICorner.Parent = Frame
+
+UIListLayout.Parent = Frame
+UIListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Center
+UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
+UIListLayout.VerticalAlignment = Enum.VerticalAlignment.Center
